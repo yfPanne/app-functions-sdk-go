3ec27ed8d8800ffc5732dc0e2f113047f3c134da
panyufeng <panyf@slpass.cloud>
2022-11-25 11:23:50 +0800

    fix block; add logs

--- internal/trigger/messagebus/messaging.go
+++ internal/trigger/messagebus/messaging.go
@@ -204,6 +204,7 @@ func (trigger *Trigger) messageHandler(logger logger.LoggingClient, _ types.Topi
 		<-mux
 	}()
 	if ok {
+		logger.Debugf("Trigger process push chain to element. chain len `%v`", chain.Len())
 		e = chain.PushBack(make(chan bool, 1))
 		context.Dic.Update(di.ServiceConstructorMap{
 			"topicChainSequence": func(get di.Get) interface{} {
@@ -217,12 +218,13 @@ func (trigger *Trigger) messageHandler(logger logger.LoggingClient, _ types.Topi
 		if processErr != nil {
 			trigger.serviceBinding.LoggingClient().Errorf("MessageBus Trigger: Failed to process message on pipeline(s): %s", processErr.Error())
 		}
-		logger.Debugf("Trigger process message over here, will trigger next sequence message.")
+		logger.Debugf("Trigger process message over here, will trigger next sequence message. chain len `%v`", chain.Len())
 		if e != nil {
 			mux <- true
 			defer func() {
 				<-mux
 			}()
+			logger.Debugf("Trigger process will trigger next `%v`", e.Next())
 			// remove element after message processing
 			if e.Next() != nil {
 				ch, ok := e.Next().Value.(chan bool)
